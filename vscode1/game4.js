ea069930af1c7cd04b45d5b0fc9aae3783d37dbf
Nimaan Buttar <168145054+NotButter18@users.noreply.github.com>
2025-03-03 19:53:21 -0500

    Delete vscode1 directory

--- vscode1/game4.js
+++
@@ -1,8 +0,0 @@
-var hexagon = document.getElementById("colourhexagon")
-var colouroptions = "0123456789abcdef";
-colouroptions = colouroptions.split("");
-
-function randomhex() {
-    var text = "#" + colouroptions[Math.floor(Math.random()*colouroptions.length)] + colouroptions[Math.floor(Math.random()*colouroptions.length)] + colouroptions[Math.floor(Math.random()*colouroptions.length)] + colouroptions[Math.floor(Math.random()*colouroptions.length)] + colouroptions[Math.floor(Math.random()*colouroptions.length)] + colouroptions[Math.floor(Math.random()*colouroptions.length)]
-    hexagon.style.backgroundColor = text
-}
